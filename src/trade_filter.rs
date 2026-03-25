7a8e1483df5d1920010264f04c832edafd0c7799
Francesco Mussin <fmussin@student.ethz.ch>
2026-03-25 19:19:32 +0100

    edited the trade_filter.rs

--- src/trade_filter.rs
+++ src/trade_filter.rs
@@ -9,6 +9,11 @@ impl TradeFilter {
 
     pub fn should_print_trade(&self, trade: &Trade) -> bool {
         let title = trade.title.as_deref().unwrap_or("").to_ascii_lowercase();
-        !title.contains("up or down") && !title.contains("temperature") && !title.contains("vs")
+        !title.contains("up or down")
+            && !title.contains("temperature")
+            && !title.contains("vs")
+            && !title.contains("Fifa")
+            && !title.contains("nba")
+            && !title.contains("f1")
     }
 }
